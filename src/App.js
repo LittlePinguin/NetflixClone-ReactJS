d04767e85126861fa4d75cbcd2aceffa2f839895
Amani <belaid.amani@sfr.fr>
2021-10-17 14:30:50 +0200

    Last Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const API_KEY = "087a55e8903a9c161c9228f783a10ad4";
+const API = "https://api.themoviedb.org/3/";
+const MOVIE_API = "https://api.themoviedb.org/3/discover/movie?api_key=087a55e8903a9c161c9228f783a10ad4&page=1";
+const TV_API = `https://api.themoviedb.org/3/tv?api_key=${API_KEY}&language=en-US`;
+const API_TEST = "https://api.themoviedb.org/3/discover/tv?api_key=087a55e8903a9c161c9228f783a10ad4"
+const IMAGES = "https://image.tmdb.org/t/p/original/";
+
 function App() {
+  const [tvShows, setTvShows] = useState([]);
+
+  useEffect(() => {
+    fetch(API_TEST)
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      setTvShows(json.results);
+      console.log(json.results);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {tvShows.map(tv => 
+        <div className="container">
+          <img src={`${IMAGES}${tv.poster_path}`} alt={tv.name}></img>
+        </div>
+      )}
     </div>
   );
 }
